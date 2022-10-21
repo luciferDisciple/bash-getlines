cf68f18e848011691e18c35f12bc2ab1f95a467c
luciferDisciple <waizer12@gmail.com>
2022-10-21 18:01:55 +0200

    unit test: test_option_e_changes_sentinel_line()

--- test/test_readlines.sh
+++ test/test_readlines.sh
@@ -2,13 +2,29 @@
 
 source src/readlines.sh
 
-test_regular_operation() {
-	local -a lines
-	local input=$'500g flour\n2 eggs\n\n'
-	readlines < <(echo -n "$input")
+is_array() {
+	local variable_name=$1
+	declare -p $variable_name 2> /dev/null | grep -q '^declare \-a'
+	return
+}
+
+test_default_behavior() {
+	readlines <<<$'500g flour\n2 eggs\n\n'
 	assertEquals '500g flour' "${REPLY[0]}"
 	assertEquals '2 eggs' "${REPLY[1]}"
 	assertEquals 2 ${#REPLY[*]}
+}
+
+test_output_variable_name_is_customizable() {
+	local input
+	input+=$'Who spilled these stars across the sky?\n'
+	input+=$'Like sparkling dust, like clouds of light.\n'
+	input+=$'\n'
+	readlines user_poem <<<"$input"
+	assertTrue 'output variable is not set' '[[ -v user_poem ]]'
+	assertTrue 'output variable is not an array' 'is_array user_poem'
+	assertEquals 'Who spilled these stars across the sky?' "${user_poem[0]}"
+	assertEquals 2 "${#user_poem[@]}"
 }
 
 test_option_p_requires_argument() {
